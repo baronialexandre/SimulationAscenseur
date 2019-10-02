62e5ccae6346eb41940ff740611d0234e4301953
FelixFeraud <felix.feraud@gmail.com>
2019-10-02 17:30:28 +0200

    Split ElevatorPanel in two for logPanel and keyboardPanel.

--- src/ui/ElevatorPanel.java
+++ src/ui/ElevatorPanel.java
@@ -3,9 +3,19 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
-public class ElevatorPanel extends JPanel {
-    public ElevatorPanel(int nbEtage) {
+public class ElevatorPanel extends JPanel
+{
+    public ElevatorPanel(int nbEtage)
+    {
         super(new BorderLayout());
         this.setBackground(new Color(105,5,254));
+        BoxLayout boxLayout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
+
+        // Ajouter votre panel ici
+
+        JPanel logPanel = new JPanel();
+
+        this.add(logPanel);
+
     }
 }
