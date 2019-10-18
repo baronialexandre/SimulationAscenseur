c2d187f59f116de63210244ab3423bf315e06fdb
kebabman <kebabman@hotmail.fr>
2019-10-19 00:13:53 +0200

    Stratégie basique non debug (elle est basique)👌👌👌👌👌✔🤣

--- src/control/strategy/BasicControlStrategy.java
+++ src/control/strategy/BasicControlStrategy.java
@@ -4,39 +4,39 @@ import control.ControlCommand;
 import utils.ControllerState;
 import utils.Direction;
 
-import java.util.Collections;
 
 import static java.lang.Thread.sleep;
 
 public class BasicControlStrategy implements ControlStrategy{
- // todo: JE SAIS PAS QUOI FAIRE COMME TRUC BASIQUE
     public void updateAimedFloor(ControlCommand controlCommand) {
         if (controlCommand.isEmergency() || controlCommand.getCallsUp().isEmpty() && controlCommand.getCallsDown().isEmpty()){
             return;
         }
-
+        System.out.println(controlCommand.getDirection().equals(Direction.UP));
         if(controlCommand.getDirection().equals(Direction.UP)){
+            System.out.println(!controlCommand.getCallsDown().isEmpty());
             if(!controlCommand.getCallsUp().isEmpty()) {
-                for (Integer i : controlCommand.getCallsUp()) {
-                    if (controlCommand.getCurrentFloor() < i) {
-                        controlCommand.setAimedFloor(i);
-                        updateElevatorState(controlCommand);
-                        return;
-                    }
-                }
+                controlCommand.setAimedFloor(controlCommand.getCallsUp().get(0));
+                updateElevatorState(controlCommand);
+                return;
             }
-            return;
+            if(!controlCommand.getCallsDown().isEmpty()) {
+                controlCommand.setAimedFloor(controlCommand.getCallsDown().get(0));
+                updateElevatorState(controlCommand);
+                return;
+            }
         }
 
         if(controlCommand.getDirection().equals(Direction.DOWN)){
             if(!controlCommand.getCallsDown().isEmpty()) {
-                for (Integer i : controlCommand.getCallsDown()) {
-                    if (controlCommand.getCurrentFloor() > i) {
-                        controlCommand.setAimedFloor(i);
-                        updateElevatorState(controlCommand);
-                        return;
-                    }
-                }
+                controlCommand.setAimedFloor(controlCommand.getCallsDown().get(0));
+                updateElevatorState(controlCommand);
+                return;
+            }
+            if(!controlCommand.getCallsUp().isEmpty()) {
+                controlCommand.setAimedFloor(controlCommand.getCallsUp().get(0));
+                updateElevatorState(controlCommand);
+                return;
             }
         }
     }
@@ -65,25 +65,21 @@ public class BasicControlStrategy implements ControlStrategy{
             controlCommand.getElevatorSimulator().setGoingNextDown();
             controlCommand.setDirection(Direction.DOWN);
             controlCommand.setState(ControllerState.MOVEMENT);
-            //System.out.println("CC direction DOWN next DOWN");
         }
         else if(controlCommand.getCurrentFloor() - controlCommand.getAimedFloor() == -1) {
             controlCommand.getElevatorSimulator().setGoingNextUp();
             controlCommand.setDirection(Direction.UP);
             controlCommand.setState(ControllerState.MOVEMENT);
-            //System.out.println("CC direction UP next UP");
         }
         else if(controlCommand.getCurrentFloor() - controlCommand.getAimedFloor() > 1) {
             controlCommand.getElevatorSimulator().setGoingDown();
             controlCommand.setDirection(Direction.DOWN);
             controlCommand.setState(ControllerState.MOVEMENT);
-            //System.out.println("CC direction DOWN");
         }
         else if(controlCommand.getCurrentFloor() - controlCommand.getAimedFloor() < -1) {
             controlCommand.getElevatorSimulator().setGoingUp();
             controlCommand.setDirection(Direction.UP);
             controlCommand.setState(ControllerState.MOVEMENT);
-            //System.out.println("CC direction UP");
         }
     }
 }
